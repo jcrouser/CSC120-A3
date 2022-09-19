9840c52f08dc47f4097e04acabe70f4612f9d821
R. Jordan Crouser <jcrouser@smith.edu>
2022-09-19 17:55:47 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+/* YOU DO NOT NEED TO MODIFY THIS FILE */
+public class Main {
+  public static void main(String[] args) {
+    /* the next line will run your Conversation bot */
+    Conversation.main(args);
+  }
+}
