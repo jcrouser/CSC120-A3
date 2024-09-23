16e8ad698e17a30295ecbd9e0a18801e4fa2cd63
reagangonzales <rgonzales@smith.edu>
2024-09-23 10:45:15 -0400

    starting

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,36 @@
+// To be able to get user input
+import java.util.Scanner;
+
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner input = new Scanner(System.in); // Scanner object
+
+    // Ask for the number of conversation rounds
+    System.out.println("How many rounds?");
+    int rounds = input.nextInt(); // Read user input
+    System.out.print(rounds);
+    
+    // Array of canned responses
+    String[] canned = {"Interesting, tell me more.",
+    "Why do you think that?",
+    "Mmm-hm.",
+    "I see.",
+    "Can you elaborate?"};
+
+    System.out.println("Hi there!  What's on your mind?");
+    // Storing transcript
+    String[] transcript = new String[rounds]; 
+    transcript[0] = "Hi there! What's on your mind?";
+
+    // Mirrored Words
+    String[] mirroredWords = {"I","you", "me", "am", "my", "your"};
+    String[] mirroredWith = {"you","I","are","your","my"};
+
+    // Conversation loop
+    for (int i = 0; i < rounds; i++) {
+      // Get user input
+      String userInput = input.nextLine();
+
   }
 }
